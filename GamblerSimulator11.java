b7e4e629ff750a874b4e8aea96f23545c658f228
Kritika <kritistarsmind@gmail.com>
2022-05-10 18:39:04 +0530

    Added:gambler simulator

---
+++ GamblerSimulator11.java
@@ -0,0 +1,15 @@
+package com.bridgelabzhw2;
+
+public class GamblerSimulator11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome Gambler");
+		
+		int Every_Day_Stack=100;
+		int Bet_Every_Game=1;
+		System.out.println("Every Day Stack:"+Every_Day_Stack);
+		System.out.println("Bet Price:"+Bet_Every_Game);
+	}
+
+}
