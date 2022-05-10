9c8688e2a3f1dbea1586f0e8a3c16910d88f7105
Kritika <kritistarsmind@gmail.com>
2022-05-10 18:52:17 +0530

    Added:Final amount

--- GamblerSimulator11.java
+++ GamblerSimulator11.java
@@ -1,15 +1,26 @@
 package com.bridgelabzhw2;
-
+import java.util.Random;
 public class GamblerSimulator11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome Gambler");
+           System.out.println("Welcome Gambler");
 		
 		int Every_Day_Stack=100;
 		int Bet_Every_Game=1;
 		System.out.println("Every Day Stack:"+Every_Day_Stack);
 		System.out.println("Bet Price:"+Bet_Every_Game);
+		
+		Random random=new Random();
+		int Bet=random.nextInt(2);
+		if(Bet==1) {
+			 System.out.println("Bet Win");
+			 System.out.println("Final Amount:"+(Every_Day_Stack+=Bet_Every_Game));
+		}
+		else {
+			 System.out.println("Bet Loss");
+			 System.out.println("Final Amount:"+(Every_Day_Stack+=Bet_Every_Game));
+		}
 	}
 
 }
